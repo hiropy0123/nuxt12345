86bf2c327c1900552f9808fbdb1d83a9fdc98974
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-10-20 14:46:07 +0900

    qiita auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,11 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/axios.js'
   ],
+  env: {
+    QIITA_TOKEN: process.env.QIITA_TOKEN
+  },
 
   /*
   ** Nuxt.js modules
