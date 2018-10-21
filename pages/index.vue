cc03334d9e3b84b7d0ceb930a969ffff0de8b97b
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-10-21 16:31:34 +0900

    auth error

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
   <section class="container">
     <div>
-      <!--  -->
+      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
+      <!-- <ul>
+        <li 
+        v-for="item in items" 
+        :key="item.id">
+          <h4>
+            <span>{{ item.title }}</span>
+            <small>by {{ item.user.id }}</small>
+          </h4>
+          <div>{{ item.body.slice(0, 130) }} ......</div>
+          <p>
+            <a :href="item.url">{{ item.url }}</a>
+          </p>
+        </li>
+      </ul> -->
     </div>
   </section>
 </template>
@@ -16,6 +30,13 @@ export default {
       JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
     )
   },
+  // async asyncData({ app }) {
+  //   const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+
+  //   return {
+  //     items
+  //   }
+  // }
 }
 </script>
 
